dbede937aa86dcb3d7882d6d9046ad146ce40c51
zsollti <zsol.pz@gmail.com>
2022-05-24 12:51:55 +0200

    init inventory table in database

--- sample_data/schema_ddl.sql
+++ sample_data/schema_ddl.sql
@@ -12,8 +12,20 @@ CREATE TABLE public.player (
     player_name text NOT NULL,
     hp integer NOT NULL,
     x integer NOT NULL,
-    y integer NOT NULL
+    y integer NOT NULL,
+    inventory_id integer NOT NULL
+);
+
+DROP TABLE IF EXISTS public.inventory;
+CREATE TABLE public.inventory (
+    id serial NOT NULL PRIMARY KEY,
+    magic_key integer,
+    magic_wand integer,
+    magic_broom integer
 );
 
 ALTER TABLE ONLY public.game_state
     ADD CONSTRAINT fk_player_id FOREIGN KEY (player_id) REFERENCES public.player(id);
+
+ALTER TABLE ONLY public.player
+    ADD CONSTRAINT fk_inventory_id FOREIGN KEY (inventory_id) REFERENCES public.inventory(id);
