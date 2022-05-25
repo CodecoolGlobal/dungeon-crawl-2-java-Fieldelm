ae8e10903be7eeaafd2d7beed170dbcd961818b2
zsollti <zsol.pz@gmail.com>
2022-05-25 12:39:03 +0200

    rewrite database init sql

--- sample_data/schema_ddl.sql
+++ sample_data/schema_ddl.sql
@@ -1,31 +1,45 @@
+ALTER TABLE IF EXISTS ONLY public.player
+    DROP CONSTRAINT IF EXISTS fk_inventory_id;
+DROP TABLE IF EXISTS public.inventory;
+
 DROP TABLE IF EXISTS public.game_state;
 CREATE TABLE public.game_state (
-    id serial NOT NULL PRIMARY KEY,
-    current_map text NOT NULL,
-    saved_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    player_id integer NOT NULL
+        id serial NOT NULL PRIMARY KEY,
+        current_map text NOT NULL,
+        saved_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
+        player_id integer NOT NULL
 );
 
 DROP TABLE IF EXISTS public.player;
 CREATE TABLE public.player (
-    id serial NOT NULL PRIMARY KEY,
-    player_name text NOT NULL,
-    hp integer NOT NULL,
-    x integer NOT NULL,
-    y integer NOT NULL,
-    inventory_id integer NOT NULL
+       id serial NOT NULL PRIMARY KEY,
+       player_name text NOT NULL,
+       hp integer NOT NULL,
+       x integer NOT NULL,
+       y integer NOT NULL
 );
 
-DROP TABLE IF EXISTS public.inventory;
-CREATE TABLE public.inventory (
-    id serial NOT NULL PRIMARY KEY,
-    magic_key integer,
-    magic_wand integer,
-    magic_broom integer
+DROP TABLE IF EXISTS public.player_items;
+CREATE TABLE public.player_items (
+       id serial NOT NULL PRIMARY KEY,
+       player_id integer NOT NULL,
+       item_name text NOT NULL
+);
+
+DROP TABLE IF EXISTS public.map_items;
+CREATE TABLE public.map_items (
+      id serial NOT NULL PRIMARY KEY,
+      map_id integer NOT NULL,
+      item_name text NOT NULL,
+      x integer NOT NULL,
+      y integer NOT NULL
 );
 
 ALTER TABLE ONLY public.game_state
     ADD CONSTRAINT fk_player_id FOREIGN KEY (player_id) REFERENCES public.player(id);
 
-ALTER TABLE ONLY public.player
-    ADD CONSTRAINT fk_inventory_id FOREIGN KEY (inventory_id) REFERENCES public.inventory(id);
+ALTER TABLE ONLY public.player_items
+    ADD CONSTRAINT fk_player_id FOREIGN KEY (player_id) REFERENCES public.player(id);
+
+ALTER TABLE ONLY public.map_items
+    ADD CONSTRAINT fk_map_id FOREIGN KEY (map_id) REFERENCES public.game_state(id);
