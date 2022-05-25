58ca5dd15dbbbdfe812597abc2bb169fb40e8ba8
zsollti <zsol.pz@gmail.com>
2022-05-25 12:42:45 +0200

    add and update functions in gameStateDaoJdbc

--- src/main/java/com/codecool/dungeoncrawl/dao/GameStateDaoJdbc.java
+++ src/main/java/com/codecool/dungeoncrawl/dao/GameStateDaoJdbc.java
@@ -9,14 +9,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GameStateDaoJdbc implements GameStateDao {
+    private final DataSource dataSource;
+
+    public GameStateDaoJdbc(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
     @Override
     public void add(GameState state) {
-
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "INSERT INTO game_state (current_map, saved_at, player_id) VALUES ( ?, ?, ?)";
+            PreparedStatement statement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, state.getCurrentMap());
+            statement.setDate(2, state.getSavedAt());
+            statement.setInt(3, state.getPlayer().getId());
+            statement.executeUpdate();
+            ResultSet resultSet = statement.getGeneratedKeys();
+            resultSet.next();
+            state.setId(resultSet.getInt(1));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void update(GameState state) {
-
+        try (Connection conn = dataSource.getConnection()){
+            String sql = "UPDATE game_state SET current_map = ?, saved_at = ?, player_id = ? WHERE id = ?";
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setString(1, state.getCurrentMap());
+            statement.setDate(2, state.getSavedAt());
+            statement.setInt(3, state.getPlayer().getId());
+            statement.executeUpdate();
+        }
+        catch(SQLException e){
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
