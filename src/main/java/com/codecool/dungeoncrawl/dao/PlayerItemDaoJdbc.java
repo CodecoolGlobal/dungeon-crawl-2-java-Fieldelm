635b5baeb4fc2dcd40d67e2c4a59e148136e8308
zsollti <zsol.pz@gmail.com>
2022-05-25 14:20:24 +0200

    make get and getAll functions in PlayerItemDaoJdbc

--- src/main/java/com/codecool/dungeoncrawl/dao/PlayerItemDaoJdbc.java
+++ src/main/java/com/codecool/dungeoncrawl/dao/PlayerItemDaoJdbc.java
@@ -1,16 +1,21 @@
 package com.codecool.dungeoncrawl.dao;
 
+import com.codecool.dungeoncrawl.model.GameState;
 import com.codecool.dungeoncrawl.model.PlayerItemModel;
+import com.codecool.dungeoncrawl.model.PlayerModel;
 
 import javax.sql.DataSource;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PlayerItemDaoJdbc implements PlayerItemDao{
     private final DataSource dataSource;
+    private final PlayerDao playerDao;
 
-    public PlayerItemDaoJdbc(DataSource dataSource) {
+    public PlayerItemDaoJdbc(DataSource dataSource, PlayerDao playerDao) {
         this.dataSource = dataSource;
+        this.playerDao = playerDao;
     }
 
     @Override
@@ -47,11 +52,46 @@ public class PlayerItemDaoJdbc implements PlayerItemDao{
 
     @Override
     public PlayerItemModel get(int id) {
-        return null;
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "SELECT player_id, item_name FROM player_items WHERE id = ?";
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            if (!resultSet.next()) {
+                return null;
+            }
+
+            PlayerModel playerModel = playerDao.get(resultSet.getInt(1));
+
+            PlayerItemModel item = new PlayerItemModel(resultSet.getString(2), playerModel);
+            item.setId(id);
+            return item;
+        } catch (SQLException e) {
+            throw new RuntimeException("Error while reading player_items with id: " + id, e);
+        }
     }
 
     @Override
     public List<PlayerItemModel> getAll() {
-        return null;
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "SELECT id, player_id, item_name FROM player_items";
+            ResultSet resultSet = conn.createStatement().executeQuery(sql);
+
+            List<PlayerItemModel> result = new ArrayList<>();
+            while (resultSet.next()) {
+                int id = resultSet.getInt(1);
+                int player_id = resultSet.getInt(2);
+                String item_name = resultSet.getString(3);
+
+                PlayerModel playerModel = playerDao.get(player_id);
+
+                PlayerItemModel item = new PlayerItemModel(item_name, playerModel);
+                item.setId(id);
+                result.add(item);
+            }
+            return result;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
