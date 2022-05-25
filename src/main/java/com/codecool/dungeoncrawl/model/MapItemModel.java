ad8e5afda5d942556d43fe02680c98e7dce4cd71
zsollti <zsol.pz@gmail.com>
2022-05-25 12:56:30 +0200

    make mapItemModel and PlayerItemModel

--- src/main/java/com/codecool/dungeoncrawl/model/MapItemModel.java
+++ src/main/java/com/codecool/dungeoncrawl/model/MapItemModel.java
@@ -1,4 +1,25 @@
 package com.codecool.dungeoncrawl.model;
 
 public class MapItemModel extends BaseModel{
+    private String name;
+    private int x;
+    private int y;
+    private GameState map;
+
+    MapItemModel(String name, int x, int y, GameState map){
+        this.name = name;
+        this.x = x;
+        this.y = y;
+        this. map = map;
+    }
+
+    public String getName(){return name;}
+    public int getX(){return x;}
+    public int getY(){return y;}
+    public GameState getMap(){return map;}
+
+    public void setName(String name){this.name = name;}
+    public void setX(int x){this.x = x;}
+    public void setY(int y){this.y = y;}
+    public void setMap(GameState map){this.map = map;}
 }
--- src/main/java/com/codecool/dungeoncrawl/model/PlayerItemModel.java
+++ src/main/java/com/codecool/dungeoncrawl/model/PlayerItemModel.java
@@ -1,4 +1,25 @@
 package com.codecool.dungeoncrawl.model;
 
-public class PlayerItemModel {
+public class PlayerItemModel extends BaseModel{
+    private String name;
+    private PlayerModel player;
+
+    public PlayerItemModel(String name, PlayerModel player){
+        this.name = name;
+        this.player = player;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public PlayerModel getPlayer(){
+        return this.player;
+    }
+    public void setPlayer(PlayerModel player){
+        this.player = player;
+    }
 }
